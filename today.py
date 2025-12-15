318a8f55858419fbd84eea1b7a1c882e4ec66fb4
caglasu-kachow <caglasu6@gmail.com>
2025-12-15 19:16:46 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,6 +1,11 @@
+"""A module computing buddy pair names for the day"""
+
+
 
 def i_am_speed():
+    """Return my favorite username"""
     return "ka-chow"
 
 def my_buddy_is():
+      """Return my buddy's name"""
     return "my_piston_cup"
